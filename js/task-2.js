1b0f064adfeddf3bf8a9741225f2a38020fdff51
IamE1off <132561973+IamE1off@users.noreply.github.com>
2023-12-18 08:27:37 +0100

    update task 2
    mentor´s recommendation

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,10 @@
 "use strict";
 function formatMessage(message, maxLength) {
-  let result;
   if (message.length <= maxLength) {
-    result = message;
+    return message;
   } else {
-    result = message.slice(0, maxLength) + "...";
+    return message.slice(0, maxLength) + "...";
   }
-  return result;
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
